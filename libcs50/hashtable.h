376220322dd8d00cb1d78860136234bada00ad57
Paolo <paolo.19@dartmouth.edu>
2017-04-27 15:06:26 -0400

    committing

--- libcs50/hashtable.h
+++ libcs50/hashtable.h
@@ -1,11 +1,10 @@
-/* 
- * hashtable.h - header file for CS50 hashtable module
+/*
+ * hashtable.h - header file for CS50 'hashtable' module
  *
- * A *hashtable* is a set of (key,item) pairs.  It acts just like a set, 
- * but is far more efficient for large collections.
+ * A 'hashtable' is a set of (key,item) pairs.
+ * It acts just like a set, but is far more efficient for large collections.
  *
- * David Kotz, April 2016, 2017
- * updated by Xia Zhou, July 2016
+ * Paolo Takagi-Atilano, April 2017
  */
 
 #ifndef __HASHTABLE_H
@@ -15,7 +14,7 @@
 #include <stdbool.h>
 
 /**************** global types ****************/
-typedef struct hashtable hashtable_t;  // opaque to users of the module
+typedef struct hashtable hashtable_t; // opaque to users of the module
 
 /**************** functions ****************/
 
@@ -23,12 +22,9 @@ typedef struct hashtable hashtable_t;  // opaque to users of the module
 hashtable_t *hashtable_new(const int num_slots);
 
 /* Insert item, identified by key (string), into the given hashtable.
- * The key string is copied for use by the hashtable; that is, the module
- * is responsible for allocating memory for a copy of the key string, and
- * later deallocating that memory; thus, the caller is free to re-use or 
- * deallocate its key string after this call.  
+ * The key string is copied for use by the hasthtable.
  * Return false if key exists in ht, any parameter is NULL, or error;
- * return true iff new item was inserted.
+ * return true if new item was inserted
  */
 bool hashtable_insert(hashtable_t *ht, const char *key, void *item);
 
@@ -38,21 +34,21 @@ bool hashtable_insert(hashtable_t *ht, const char *key, void *item);
 void *hashtable_find(hashtable_t *ht, const char *key);
 
 /* Print the whole table; provide the output file and func to print each item.
- * Ignore if NULL fp. Print (null) if NULL ht. 
+ * Ignore if NULL fp. Print (null) if NULL ht.
  * Print a table with no items if NULL itemprint.
  */
-void hashtable_print(hashtable_t *ht, FILE *fp, 
-		     void (*itemprint)(FILE *fp, const char *key, void *item));
+void hashtable_print(hashtable_t *ht, FILE *fp,
+			void (*itemprint)(FILE *fp, const char *key, void *item) );
 
 /* Iterate over all items in the table; in undefined order.
  * Call the given function on each item, with (arg, key, item).
  * If ht==NULL or itemfunc==NULL, do nothing.
  */
 void hashtable_iterate(hashtable_t *ht, void *arg,
-	       void (*itemfunc)(void *arg, const char *key, void *item) );
+			void (*itemfunc)(void *arg, const char *key, void *item) );
 
 /* Delete the whole hashtable; ignore NULL ht.
- * Provide a function that will delete each item (may be NULL).
+ * Provide a function that will delete each item (may be NULL)/
  */
 void hashtable_delete(hashtable_t *ht, void (*itemdelete)(void *item) );
 
